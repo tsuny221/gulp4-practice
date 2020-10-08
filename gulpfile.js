cd1776f8eea0f7179c6bd17e8174ddfae11f7e5b
tsuny221 <kingoftsuny@gmail.com>
2020-10-08 15:27:59 +0900

    [Update] practice imageresize imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
-const { src, dest } = require("gulp");
-//{src, dest} gulpでやるよりこの方が記述がsrcのみでOK
-const rename = require("gulp-rename"); //ファイル名を変更する
-
-function copyFiles() {
-  return src("./src/index.html")
-    .pipe(
-      rename({
-        prefix: "hello-",
-      })
-    )
-    .pipe(dest("./dist"));
+const { src, dest } = require("gulp"); //{src, dest} gulpでやるよりこの方が記述がsrcのみでOK
+const imagemin = require("gulp-imagemin");
+const loadPlugins = require("gulp-load-plugins"); //まとめて読み込む方法
+const $ = loadPlugins();
+const pkg = require("./package.json");
+const conf = pkg["gulp-config"];
+const sizes = conf.sizes;
+function icon(done) {
+  //コールバック関数（for文ではreturnが使えないので）
+  for (let size of sizes) {
+    let width = size[0];
+    let height = size[1];
+    src("./favicon.png")
+      .pipe(
+        $.imageResize({
+          width,
+          height,
+          crop: true,
+          unscale: false,
+        }) //サイズ変更
+      )
+      .pipe($.imagemin()) //画像圧縮
+      .pipe($.rename(`favicon-${width}x${height}.png`))
+      .pipe(dest("./dist/images/icon"));
+  }
+  done();
 }
-
-exports.copyFiles = copyFiles;
+exports.icon = icon;
 
 //distにファイルがコピーされる配列で複数のファイル、フォルダに指定も可能
 //"./src/*"ワイルドカードでフォルダ内のもの全てコピーも可能
